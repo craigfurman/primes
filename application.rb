38256e52b701f1f428672398892d96c4b2e6411a
Craig Furman <craig.furman89@gmail.com>
2014-03-16 11:08:35 +0000

    application includes comment blocks to toggle fibonacci/prime number sequences for multiplication table

--- application.rb
+++ application.rb
@@ -3,8 +3,13 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
 require 'multiplication_table'
 require 'table_presenter'
 
+# comment/uncomment to use prime numbers as sequence
 require 'sieve_of_eratosthenes'
 sequence_generator = SieveOfEratosthenes.new
+
+# comment/uncomment to use fibonacci numbers as sequence
+# require 'fibonacci_numbers'
+# sequence_generator = FibonacciNumbers.new
 
 multiplication_table = MultiplicationTable.new
 table_presenter = TablePresenter.new(multiplication_table, $stdout)
